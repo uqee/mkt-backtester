0b12bf258488569944307500adb37669abe18f34
Denis Zhbankov <denis.zhbankov@gmail.com>
2015-11-28 16:24:07 +0300

    orders: fix sl and tp execution

--- run.js
+++ run.js
@@ -5,5 +5,5 @@ var lib = require('./lib');
 var data = lib.quotes.parse(lib.quotes.read(process.argv[2]));
 var system = require(process.argv[3]);
 
-var res = system(data, process.argv.slice(4));
-fs.writeFileSync('__result', res);
+var pnl = system(data, process.argv.slice(4));
+fs.writeFileSync('__result', pnl);
